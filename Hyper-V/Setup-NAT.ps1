36319b0e93b8679f86c688faa4d527bd16477246
Simon Rüegg <simon@rueggs.ch>
2017-03-19 15:35:35 +0100

    Update NAT creation script

--- Hyper-V/Setup-NAT.ps1
+++ Hyper-V/Setup-NAT.ps1
@@ -1,12 +1,17 @@
-# 1. Enable sharing for physical ethernet adapter
-# 2. Add NAT for Hyper-V internal switch
-New-NetNat –Name 'HyperV_NATnetwork' –InternalIPInterfaceAddressPrefix 192.168.137.0/24
+# 1. Create internal network switch
+New-VMSwitch –SwitchName "NATSwitch" –SwitchType Internal
 
-# 3. Port forwarding host:1022 => linux1(192.168.137.18):22
-Add-NetNatStaticMapping -NatName 'HyperV_NATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 192.168.137.18 -InternalPort 22 -ExternalPort 1022
+# 2. Set IP address
+New-NetIPAddress –IPAddress 172.21.21.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NATSwitch)"
 
-# 4. Port forwarding host:2022 => linux2(192.168.137.95):22
-Add-NetNatStaticMapping -NatName 'HyperV_NATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 192.168.137.95 -InternalPort 22 -ExternalPort 2022
+# 3. Add NAT for Hyper-V internal switch
+New-NetNat –Name 'MyNATnetwork' –InternalIPInterfaceAddressPrefix 172.21.21.0/24
 
-# 5. Port forwarding host:3022 => linux3(192.168.137.213):22
-Add-NetNatStaticMapping -NatName 'HyperV_NATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 192.168.137.213 -InternalPort 22 -ExternalPort 3022
+# 4. Port forwarding host:1022 => linux1(172.21.21.):22
+Add-NetNatStaticMapping -NatName 'MyNATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.21.21.101 -InternalPort 22 -ExternalPort 1022
+
+# 5. Port forwarding host:2022 => linux2(172.21.21.102):22
+Add-NetNatStaticMapping -NatName 'MyNATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.21.21.102 -InternalPort 22 -ExternalPort 2022
+
+# 6. Port forwarding host:3022 => linux3(172.21.21.103):22
+Add-NetNatStaticMapping -NatName 'MyNATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.21.21.103 -InternalPort 22 -ExternalPort 3022

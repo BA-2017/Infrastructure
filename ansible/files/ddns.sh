bb419b387835a1298549ff234c5e6b0825e9d170
Simon Rüegg <simon@rueggs.ch>
2017-03-20 15:22:29 +0100

    Update ddns to not contain DNSimple token

--- ansible/files/ddns.sh
+++ ansible/files/ddns.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TOKEN="4BxFCzfeFBv2LvMWPv1o4YT8ew38fR87"  # The API v2 OAuth token
+TOKEN="$DNSimpleToken"  # The API v2 OAuth token
 ACCOUNT_ID="52675"        # Replace with your account ID
 ZONE_ID="zhaw.tk"  # The zone ID is the name of the zone (or domain)
 RECORD_ID="{RECORD_ID}"       # Replace with the Record ID
@@ -12,5 +12,7 @@ curl -s -H "Authorization: Bearer $TOKEN" \
      -H "Content-Type: application/json" \
      -H "Accept: application/json" \
      -X "PATCH" \
-     -i "https://api.dnsimple.com/v2/$ACCOUNT_ID/zones/$ZONE_ID/records/$RECORD_ID" \
-     -d "{\"content\":\"$IP\"}" > /dev/null
+     "https://api.dnsimple.com/v2/$ACCOUNT_ID/zones/$ZONE_ID/records/$RECORD_ID" \
+     -d "{\"content\":\"$IP\"}" \
+
+printf "\n"

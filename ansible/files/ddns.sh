c525ae6ebbed2edd6bf294ba7cd2da66d7e82a83
Simon Rüegg <simon@rueggs.ch>
2017-03-24 18:26:02 +0100

    Provide DNS record id in env var

--- ansible/files/ddns.sh
+++ ansible/files/ddns.sh
@@ -3,7 +3,7 @@
 TOKEN="$DNSimpleToken"  # The API v2 OAuth token
 ACCOUNT_ID="52675"        # Replace with your account ID
 ZONE_ID="zhaw.tk"  # The zone ID is the name of the zone (or domain)
-RECORD_ID="{RECORD_ID}"       # Replace with the Record ID
+RECORD_ID="$DNSimpleRecordID"       # Replace with the Record ID
 IP=`/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`
 
 echo "`date '+%FT%T'`  Set DNS record for '`hostname`' to '$IP'"

eba70f6b3adf59df0d78e2886f00117cf37165d6
Simon Rüegg <simon@rueggs.ch>
2017-03-27 11:36:10 +0200

    Add port forwarding for kubernetes API server

--- Hyper-V/Setup-NAT.ps1
+++ Hyper-V/Setup-NAT.ps1
@@ -7,14 +7,17 @@ New-NetIPAddress –IPAddress 172.21.21.1 -PrefixLength 24 -InterfaceAlias "vEth
 # Add NAT for Hyper-V internal switch
 New-NetNat –Name 'MyNATnetwork' –InternalIPInterfaceAddressPrefix 172.21.21.0/24
 
-# Port forwarding host:1022 => linux1(172.21.21.):22
-Add-NetNatStaticMapping -NatName 'MyNATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.21.21.101 -InternalPort 22 -ExternalPort 1022
+# SSH port forwarding host:1022 => linux1(172.21.21.101):22
+Add-NetNatStaticMapping -NatName 'MyNATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.21.21.101 -Internalp 22 -Externalp 1022
 
-# Port forwarding host:2022 => linux2(172.21.21.102):22
-Add-NetNatStaticMapping -NatName 'MyNATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.21.21.102 -InternalPort 22 -ExternalPort 2022
+# SSH port forwarding host:2022 => linux2(172.21.21.102):22
+Add-NetNatStaticMapping -NatName 'MyNATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.21.21.102 -Internalport 22 -ExternalPort 2022
 
-# Port forwarding host:3022 => linux3(172.21.21.103):22
-Add-NetNatStaticMapping -NatName 'MyNATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.21.21.103 -InternalPort 22 -ExternalPort 3022
+# SSH port forwarding host:3022 => linux3(172.21.21.103):22
+Add-NetNatStaticMapping -NatName 'MyNATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.21.21.103 -Internalport 22 -ExternalPort 3022
 
-# Port forwarding host:3399 => windows1(172.21.21.201):3389
-Add-NetNatStaticMapping -NatName 'MyNATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.21.21.201 -InternalPort 3389 -ExternalPort 3399
+# RDP port forwarding host:3399 => windows1(172.21.21.201):3389
+Add-NetNatStaticMapping -NatName 'MyNATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.21.21.201 -Internalport 3389 -ExternalPort 3399
+
+# Kubernetes API-Server Port forwarding host:6443 => master(172.21.21.101):6443
+Add-NetNatStaticMapping -NatName 'MyNATnetwork' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.21.21.101 -Internalp 6443 -Externalp 6443
